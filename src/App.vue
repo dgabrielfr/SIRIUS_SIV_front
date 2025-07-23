f8ea20ad307639250ccf660892f34cdcaa7eb0f9
Damien GABRIEL <damien.gabriel.fr@gmail.com>
2025-07-23 16:05:06 +0200

    Création du front et d'une route pour tester la communication entre le front et le back

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+
+let data = ref()
+async function hello(){
+  await axios.get('http://127.0.0.1:6543/hello').then(function (response){
+    console.log(response.data)
+    data.value = response.data
+  })
+}
+</script>
+
+<template>
+  <div>
+    <button @click="hello">Appel de la route hello du backend</button>
+    <p v-if="data">Données renvoyées par le backend : {{ data }}</p>
+  </div>
+</template>
